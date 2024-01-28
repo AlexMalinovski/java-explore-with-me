1d0e1443aee1cfd0542804a2bc787db0e51c02a9
Alexey Malinovsky <alexmal08@yandex.ru>
2024-01-28 23:37:29 +0300

    style: fix

--- exception-handler-spring-boot-starter/src/main/java/ru/practicum/exceptions/ProblemDetail.java
+++ exception-handler-spring-boot-starter/src/main/java/ru/practicum/exceptions/ProblemDetail.java
@@ -2,7 +2,6 @@ package ru.practicum.exceptions;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AccessLevel;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
